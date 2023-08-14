1b6f57f3ea94c16ace93b2a01a664754b5061ddf
harikrishnait <caharikrishnag@gmail.com>
2023-08-14 09:05:15 +0530

    refactor: cors

--- index.js
+++ index.js
@@ -5,21 +5,14 @@ const cors = require("cors");
 const healthRoute = require("./routes/HealthRoute");
 const authRoutes = require("./routes/AuthRoutes");
 
-var whitelist = ["http://localhost:5173" /** other domains if any */];
-var corsOptions = {
-  credentials: true,
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
 const app = express();
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["https://awaas-vishwa-hari.vercel.app"],
+  })
+);
 
 mongoose.connect(process.env.DATABASE_URL);
 mongoose.connection.once("connected", () => console.log("Database Connected"));
@@ -27,7 +20,3 @@ mongoose.connection.on("error", (er) => console.log("Database Error:", er));
 
 app.use("/health", healthRoute);
 app.use("/api/v1/auth", authRoutes);
-
-app.listen(process.env.SERVER_PORT, () =>
-  console.log("app server started at 3000 port")
-);
